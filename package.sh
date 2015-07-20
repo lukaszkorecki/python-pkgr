84767c34db072e9948ce7c674f88e3c1903f36a3
Łukasz Korecki <lukasz@korecki.me>
2015-07-20 09:16:35 +0000

    remove unnecessary stuff

--- package.sh
+++ package.sh
@@ -5,10 +5,6 @@ cacheRoot=~/.pkgr_cache
 environment="'HOME=$HOME' 'SSH_AUTH_SOCK=$SSH_AUTH_SOCK' 'PKGR=1'"
 pkgr package . \
   --env ${environment} \
-  --user=deploy \
-  --group=deploy \
   --buildpack=git@github.com:heroku/heroku-buildpack-python.git \
-  --runner=upstart-1.5 \
-  --version="$(cat VERSION)" \
   --compile-cache-dir=${cacheRoot}/py-compile \
   --buildpacks-cache-dir=${cacheRoot}/py-build
